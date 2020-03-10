a00e47a25e72971c8e92a356f4e3359a5997f242
vinicius_dev <viniciuscarrascal@hotmail.com>
2020-03-09 23:25:17 -0300

    adicionando css em arquivo separado

--- bootstrap/desafios/googledrive/vinicius/index.php
+++ bootstrap/desafios/googledrive/vinicius/index.php
@@ -12,7 +12,7 @@
             crossorigin="anonymous"></script>
     <title>Meu Drive - Google Drive</title>
     <link href='https://www.gstatic.com/images/branding/product/1x/drive_48dp.png' rel='icon' type=''/>
-
+    <link rel="stylesheet" href="index.css">
 </head>
 <body>
 <div class="container-fluid border-bottom sticky-top bg-white">
@@ -20,22 +20,19 @@
         <div class="col-2">
             <nav class="navbar navbar-white">
                 <a><img src="https://www.gstatic.com/images/branding/product/1x/drive_48dp.png" alt="drive"><small
-                            style="font-size: 25px;" class="ml-2">Drive</small></a>
+                            id="fonts-size" class="ml-2">Drive</small></a>
             </nav>
         </div>
         <div class="col-7">
-            <div class="input-group border mt-2 ml-2">
-                <span class="input-group-btn">
-                    <button class="btn btn-default bg-light" type="button"><i class="fas fa-search"></i></button>
-                </span>
-                <input class="form-control bg-light" type="search" placeholder="Pesquisar no Drive" arial-label="Search"
-                       style="border: none">
-                <span class="input-group-btn">
-                    <button class="btn btn-default bg-light" type="button"><i class="fas fa-sort-down"></i></button>
-                </span>
+            <div class="input-group mt-2 ml-2 border" id="cor-input-nav">
+                <button id="form1" class="btn btn-default bg-light" type="button"><i class="fas fa-search"></i></button>
+                <input id="form" class="form-control bg-light" type="search" placeholder="Pesquisar no Drive"
+                       arial-label="Search"/>
+                <button id="form1" class="btn btn-default bg-light" type="button"><i class="fas fa-sort-down"></i>
+                </button>
             </div>
         </div>
-        <div class="col mt-2 d-flex justify-content-end" style="font-size: 25px">
+        <div class="col mt-2 d-flex justify-content-end" id="fonts-size">
             <div class="mr-5">
                 <i class="far fa-question-circle mr-2"></i>
                 <i class="fas fa-cog"></i>
@@ -52,46 +49,39 @@
         <div class="col-2">
             <div class="row">
                 <div class="m-2">
-                    <div class="btn shadow p-3 bg-white" style="border-radius: 100px; width: 130px;">
-                        <span class="card-text mt-2" style="font-size: 20px"><i
+                    <div class="btn shadow p-3 bg-white" id="novo-buttom">
+                        <span class="card-text mt-2" id="fonts-size"><i
                                     class="fas fa-plus mx-2"></i><small>Novo</small></span>
                     </div>
                 </div>
             </div>
             <div class="row">
-                <div class="btn btn-light pr-5 w-100"
-                     style="border-bottom-right-radius: 50px; border-top-right-radius: 50px; background-color: #e8f0fe">
-                    <a id="drive" href="meu-drive" style="text-decoration: none; color: blue"
+                <div class="btn btn-light pr-5 w-100" id="button-lateral-azul">
+                    <a id="link-buttom" href="meu-drive"
                        class="card-text d-flex justify-content-start">
                         <span><i class="mt-1 fas fa-caret-right"></i><i class="ml-2 mt-1 fab fa-google mr-2"></i><small>Meu Drive</small></span>
                     </a>
                 </div>
-                <div class="btn btn-light pr-5 w-100"
-                     style="border-bottom-right-radius: 50px; border-top-right-radius: 50px;">
-                    <a id="drive" href="compartilhados-comigo" style="text-decoration: none; color: black"
+                <div class="btn btn-light pr-5 w-100" id="button-lateral">
+                    <a id="link-buttom-preto" href="compartilhados-comigo"
                        class="card-text d-flex justify-content-start">
                         <span><i class="mt-1 fas fa-caret-right"></i><i
                                     class="fas fa-user-friends ml-2 mr-1"></i><small>Compartilhados Comigo</small></span>
                     </a>
                 </div>
-                <div class="btn btn-light w-100"
-                     style="border-bottom-right-radius: 50px; border-top-right-radius: 50px;">
-                    <a id="drive" href="recentes" style="text-decoration: none; color: black"
+                <div class="btn btn-light w-100" id="button-lateral">
+                    <a id="link-buttom-preto" href="recentes"
                        class="card-text d-flex justify-content-start">
                         <span><i class="mt-1 fas fa-caret-right"></i><i class="ml-2 far fa-clock mr-1"></i><small>Recentes</small></span>
                     </a>
                 </div>
-                <div class="btn btn-light w-100"
-                     style="border-bottom-right-radius: 50px; border-top-right-radius: 50px;">
-                    <a id="drive" href="com-estrelas" style="text-decoration: none; color: black"
-                       class="card-text d-flex justify-content-start">
+                <div class="btn btn-light w-100" id="button-lateral">
+                    <a id="link-buttom-preto" href="com-estrelas" class="card-text d-flex justify-content-start">
                         <span><i class="mt-1 fas fa-caret-right"></i><i class="far fa-star ml-2 mr-1"></i><small>Com Estrela</small></span>
                     </a>
                 </div>
-                <div class="btn btn-light w-100"
-                     style="border-bottom-right-radius: 50px; border-top-right-radius: 50px;">
-                    <a id="drive" href="lixeira" style="text-decoration: none; color: black"
-                       class="card-text d-flex justify-content-start">
+                <div class="btn btn-light w-100" id="button-lateral">
+                    <a id="link-buttom-preto" href="lixeira" class="card-text d-flex justify-content-start">
                         <span><i class="mt-1 fas fa-caret-right"></i><i class="far fa-trash-alt mr-1 ml-2"></i><small>Lixeira</small></span>
                     </a>
                 </div>
@@ -103,7 +93,7 @@
                         </div>
                     </div>
                     <div class="ml-5">
-                        <div class="progress ml-3" style="height: 5px; width: 160px">
+                        <div class="progress ml-3" id="progressbar">
                             <div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25"
                                  aria-valuemin="0" aria-valuemax="100"></div>
                         </div>
@@ -113,7 +103,7 @@
                         <div class="ml-2 mt-2">
                             <div class="card">
                                 <div class="btn btn-light">
-                                    <p class="card-text"<a href="" style="text-decoration: none"><small>Comprar
+                                    <p class="card-text"<a href="" id="link-buttom-preto"><small>Comprar
                                             Armazenamento</small></a></p>
                                 </div>
                             </div>
@@ -125,15 +115,15 @@
         <div class="col">
             <div class="container-fluid">
                 <div class="row border-bottom">
-                    <div class="dropdown">
-                        <button type="button" class="btn btn-link mt-2 dropdown-toggle"
-                                style="text-decoration: none; color: black;">Meu Drive
-                        </button>
+                    <div class="mt-2">
+                        <div class="row ml-1">
+                            <button type="button" class="btn btn-link dropdown-toggle" id="link-buttom-preto">Meu Drive</button>
+                        </div>
                     </div>
                 </div>
                 <small><b>Acesso Rápido</b></small>
                 <div class="row">
-                    <?php for ($i > 0; $i < 4; $i++) { ?>
+                    <?php for ($i = 0; $i < 4; $i++) { ?>
                         <div class="col-3">
                             <div class="card" style="width: 13rem;">
                                 <div class="border border-primary">
@@ -149,7 +139,7 @@
                 </div>
                 <small><b>Pastas</b></small>
                 <div class="row">
-                    <?php for ($a > 0; $a < 8; $a++) { ?>
+                    <?php for ($a = 0; $a < 8; $a++) { ?>
                         <div class="col-3 mt-2">
                             <div class="card">
                                 <div class="btn btn-light">
@@ -204,7 +194,7 @@
             </div>
         </div>
         <div class="border">
-            <div class="ml-3 mr-3" style="font-size: 30px">
+            <div class="ml-3 mr-3" id="fonts-sizem">
                 <div class="mt-3">
                     <i class="far fa-grin-alt"></i>
                 </div>
