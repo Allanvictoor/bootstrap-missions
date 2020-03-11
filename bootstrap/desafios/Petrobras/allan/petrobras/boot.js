bc4c60b1a00609a6497a4afb2a1712643b78072e
Allan_vwd <allanbatista@vivaweb.net>
2020-03-11 16:51:49 -0300

    diagramação da pagina petrobras

---
+++ bootstrap/desafios/Petrobras/allan/petrobras/boot.js
@@ -0,0 +1,11 @@
+
+
+
+
+bowser.mobile = bowser.mobile ? true : false;
+bowser.tablet = bowser.tablet ? true : false;
+//console.log("boot> initialize - phone["+bowser.mobile+"] tablet["+bowser.tablet+"]");
+//console.log("bowser> ",bowser);
+Application.initialize();
+
+
