4710f1ebd89f981a05fea670e1ca1ff98c9d6fe6
vinicius_dev <viniciuscarrascal@hotmail.com>
2020-03-18 00:15:26 -0300

    alterações para envio de informações do formulario

--- bootstrap/desafios/ouvidoria/vinicius/ouvidoria.php
+++ bootstrap/desafios/ouvidoria/vinicius/ouvidoria.php
@@ -10,7 +10,7 @@
 include("../vinicius/nav.php")
 ?>
 <div class="container my-3">
-    <form action="../vinicius/banco.php">
+    <form method="post" action="../vinicius/banco.php">
         <div class="form-group">
             <div class="panel panel-primary">
                 <div class="panel-heading">
@@ -21,56 +21,31 @@ include("../vinicius/nav.php")
                 <div class="panel-body">
                     <div class="row">
                         <div class="col-md-12 col-sm-12 my-3">
-                            <label>Nome Completo *</label>
-                            <input class="w-100 form-control" type="text" required/>
+                            <label>Nome *</label>
+                            <input class="w-100 form-control" type="text" placeholder="Digite seu nome completo" name="nome" required/>
                         </div>
                         <div class="col-md-6 col-sm-12">
                             <div class="form-group">
                                 <label for="formPrincipal:dtNascimento_Input">Data de nascimento</label>
                                 <div class="input-group date" id="formPrincipal:dtNascimento">
-                                    <input type="text" id="nascimento" name="formPrincipal:dtNascimento"
+                                    <input type="text" id="nascimento" name="nascimento"
                                            class="form-control" required>
                                 </div>
                             </div>
                         </div>
                         <div class="col-md-6 col-sm-12">
                             <label>E-mail</label>
-                            <input id="email" class="w-100 form-control" type="email" required/>
+                            <input id="email" class="w-100 form-control" type="email" name="email" placeholder="Digite seu Email" required/>
                         </div>
                     </div>
                     <div class="row">
                         <div class="col-md-6 col-sm-12">
-                            <label>CPF/CNPJ</label>
-                            <input id="cpf" class="w-100 form-control" type="text" required/>
+                            <label>CPF</label>
+                            <input id="cpf" class="w-100 form-control" type="text" placeholder="Digite seu CPF" name="cpf" required/>
                         </div>
                         <div class="col-md-6 col-sm-12">
                             <label>RG</label>
-                            <input id="rg" class="w-100 form-control" type="text" required/>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-6 col-sm-12 my-3">
-                            <div class="form-group">
-                                <fieldset>
-                                    <legend>Informe o sexo</legend>
-                                    <table id="formPrincipal:sexo">
-                                        <tbody>
-                                        <tr>
-                                            <td>
-                                                <input type="radio" name="formPrincipal:sexo" id="formPrincipal:sexo:0"
-                                                       value="M"><label for="formPrincipal:sexo:0"> Masculino</label>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td>
-                                                <input type="radio" name="formPrincipal:sexo" id="formPrincipal:sexo:1"
-                                                       value="F"><label for="formPrincipal:sexo:1"> Feminino</label>
-                                            </td>
-                                        </tr>
-                                        </tbody>
-                                    </table>
-                                </fieldset>
-                            </div>
+                            <input id="rg" class="w-100 form-control" type="text" placeholder="Digite seu RG" name="rg" required/>
                         </div>
                     </div>
                     <div class="border-bottom border-top d-flex justify-content-center">
@@ -79,36 +54,36 @@ include("../vinicius/nav.php")
                     <div class="row mt-3">
                         <div class="col-md-4 col-sm-12">
                             <label>CEP</label>
-                            <input id="cep" class="w-100 form-control" type="text" required/>
+                            <input id="cep" class="w-100 form-control" type="text" placeholder="Digite o CEP" name="cep" required/>
                         </div>
                         <div class="col-md-4">
                             <label>Logradouro</label>
-                            <input class="w-100 form-control form-control" type="text" required/>
+                            <input class="w-100 form-control form-control" type="text" name="logradouro" placeholder="Digite o logradouro" required/>
                         </div>
                         <div class="col-md-4 col-sm-12">
                             <label>Número</label>
-                            <input class="w-100 form-control" type="text" required/>
+                            <input class="w-100 form-control" type="text" placeholder="Número" name="numero" required/>
                         </div>
                     </div>
                     <div class="row mt-3">
                         <div class="col-md-4 col-sm-12">
                             <label>Complemento</label>
-                            <input class="w-100 form-control" type="text" required/>
+                            <input class="w-100 form-control" type="text" placeholder="Complemento" name="complemento" required/>
                         </div>
                         <div class="col-md-4 col-sm-12">
                             <label>Bairro</label>
-                            <input class="w-100 form-control" type="text" required/>
+                            <input class="w-100 form-control" type="text" name="bairro" placeholder="Bairro" required/>
                         </div>
                         <div class="col-md-4 col-sm-12">
                             <label>Cidade</label>
-                            <input class="w-100 form-control" type="text" required/>
+                            <input class="w-100 form-control" type="text" name="cidade" placeholder="Cidade" required/>
                         </div>
                     </div>
                     <div class="row mt-3">
                         <div class="col-md-4 col-sm-12">
                             <label>UF</label>
                             <div class="form-group " id="formPrincipal:uf">
-                                <select id="formPrincipal:ufInner" name="formPrincipal:ufInner"
+                                <select id="formPrincipal:ufInner" name="estados"
                                         class="form-control"
                                         required>
                                     <option data-label="Selecione" value="">Selecione</option>
@@ -144,12 +119,12 @@ include("../vinicius/nav.php")
                         </div>
                         <div class="col-md-4 col-sm-12">
                             <label>País</label>
-                            <input class="w-100 form-control" type="text" required/>
+                            <input class="w-100 form-control" type="text" placeholder="País" name="país" required/>
                         </div>
                         <div class="col-md-4 col-sm-12">
                             <div class="form-group">
                                 <label for="">Celular</label>
-                                <input id="celular" name="formPrincipal:celular" type="text"
+                                <input id="celular" name="celular" placeholder="(99) 9 9999-9999 " type="text"
                                        maxlength="15" class="form-control"
                                        role="textbox" aria-disabled="false" aria-readonly="false" required>
                             </div>
@@ -165,14 +140,14 @@ include("../vinicius/nav.php")
                                     de manifestação *</label>
                                 <div class="form-group " id="formPrincipal:tipoManifestacao">
                                     <select id="formPrincipal:tipoManifestacaoInner"
-                                            name="formPrincipal:tipoManifestacaoInner"
+                                            name="tipomanifestacao"
                                             class="form-control  bf-required" required>
                                         <option data-label="Selecione" value="">Selecione</option>
-                                        <option data-label="Denúncia" value="1">Denúncia</option>
-                                        <option data-label="Elogio" value="2">Elogio</option>
-                                        <option data-label="Reclamação" value="4">Reclamação</option>
-                                        <option data-label="Solicitação" value="5">Solicitação</option>
-                                        <option data-label="Sugestão" value="6">Sugestão</option>
+                                        <option data-label="Denúncia" value="Denúncia">Denúncia</option>
+                                        <option data-label="Elogio" value="Elogio">Elogio</option>
+                                        <option data-label="Reclamação" value="Reclamação">Reclamação</option>
+                                        <option data-label="Solicitação" value="Solicitação">Solicitação</option>
+                                        <option data-label="Sugestão" value="Sugestão">Sugestão</option>
                                     </select>
                                 </div>
                             </div>
@@ -183,13 +158,13 @@ include("../vinicius/nav.php")
                                     relacionamento conosco</label>
                                 <div class="form-group " id="formPrincipal:relacionamento">
                                     <select id="formPrincipal:relacionamentoInner"
-                                            name="formPrincipal:relacionamentoInner"
+                                            name="relacionamento"
                                             class="form-control" required>
                                         <option data-label="Selecione" value="">Selecione</option>
-                                        <option data-label="Juiz do Trabalho" value="3">Aluno</option>
-                                        <option data-label="Estagiário" value="2">Funcionário</option>
-                                        <option data-label="Advogado" value="1">Professor</option>
-                                        <option data-label="Outros" value="4">Outros</option>
+                                        <option data-label="Juiz do Trabalho" value="Aluno">Aluno</option>
+                                        <option data-label="Estagiário" value="Funcionário">Funcionário</option>
+                                        <option data-label="Advogado" value="Professor">Professor</option>
+                                        <option data-label="Outros" value="Outros">Outros</option>
                                     </select></div>
                             </div>
                         </div>
@@ -198,7 +173,7 @@ include("../vinicius/nav.php")
                         <div class="form-group mb-3">
                             <div class="col-md-6 col-sm-12">
                                 <label for="formPrincipal:fileUploader">Anexar arquivo</label>
-                                <input id="formPrincipal:fileUploader" type="file" name="formPrincipal:fileUploader">
+                                <input id="formPrincipal:fileUploader" type="file" name="imagem">
                             </div>
                         </div>
                     </div>
@@ -207,7 +182,7 @@ include("../vinicius/nav.php")
                             <div class="form-group">
                                 <label for="">Texto
                                     Manifestação *</label>
-                                <textarea id="" name="Manifestacao" class="form-control" maxlength="255">
+                                <textarea id="" name="manifestacao" class="form-control" maxlength="255">
                             </textarea>
                             </div>
                         </div>
