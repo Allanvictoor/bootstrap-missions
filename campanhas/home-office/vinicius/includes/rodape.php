f304971be4ddac9c40b472fa76e1a65dd8a5123c
vinnynh <viniciuscarrascal@hotmail.com>
2020-04-04 00:42:05 -0300

    deixando footer com links

--- campanhas/home-office/vinicius/includes/rodape.php
+++ campanhas/home-office/vinicius/includes/rodape.php
@@ -8,16 +8,16 @@
                         <div class="row">
                             <div class="d-block mt-3">
                                 <div class="col-md-12 ml-3 border-left-footer">
-                                    <p>Start</p>
+                                    <p><a href="#">Start</a></p>
                                 </div>
                                 <div class="col-md-12 ml-3 border-left-footer">
-                                    <p>Experimente, é Grátis</p>
+                                    <p><a href="#">Experimente, é Grátis</a></p>
                                 </div>
                                 <div class="col-md-12 ml-3 border-left-footer">
-                                    <p>Ajuda</p>
+                                    <p><a href="#">Ajuda</a></p>
                                 </div>
                                 <div class="col-md-12 ml-3 border-left-footer">
-                                    <p>Treinamentos ao Vivo</p>
+                                    <p><a href="#">Treinamentos ao Vivo</a></p>
                                 </div>
                             </div>
                         </div>
@@ -31,25 +31,25 @@
                         <div class="row">
                             <div class="d-block mt-3">
                                 <div class="col-md-12 ml-3 border-left-footer">
-                                    <p>Enterprise</p>
+                                    <p><a href="#">Enterprise</a></p>
                                 </div>
                                 <div class="col-md-12 ml-3 border-left-footer">
-                                    <p>Comunicação</p>
+                                    <p><a href="#">Comunicação</a></p>
                                 </div>
                                 <div class="col-md-12 ml-3 border-left-footer">
-                                    <p>Colaboração</p>
+                                    <p><a href="#">Colaboração</a></p>
                                 </div>
                                 <div class="col-md-12 ml-3 border-left-footer">
-                                    <p>Produtividade</p>
+                                    <p><a href="#">Produtividade</a></p>
                                 </div>
                                 <div class="col-md-12 ml-3 border-left-footer">
-                                    <p>Recursos Humanos</p>
+                                    <p><a href="#">Recursos Humanos</a></p>
                                 </div>
                                 <div class="col-md-12 ml-3 border-left-footer">
-                                    <p>Extranet</p>
+                                    <p><a href="#">Extranet</a></p>
                                 </div>
                                 <div class="col-md-12 ml-3 border-left-footer">
-                                    <p>Contato</p>
+                                    <p><a href="#">Contato</a></p>
                                 </div>
                             </div>
                         </div>
@@ -58,22 +58,22 @@
                         <div class="row">
                             <div class="d-block mt-3">
                                 <div class="col-md-12 ml-3 border-left-footer">
-                                    <p>Preços</p>
+                                    <p><a href="#">Preços</a></p>
                                 </div>
                                 <div class="col-md-12 ml-3 border-left-footer">
-                                    <p>Blog</p>
+                                    <p><a href="#">Blog</a></p>
                                 </div>
                                 <div class="col-md-12 ml-3 border-left-footer">
-                                    <p>Colaboração</p>
+                                    <p><a href="#">Colaboração</a></p>
                                 </div>
                                 <div class="col-md-12 ml-3 border-left-footer">
-                                    <p>Comunicação</p>
+                                    <p><a href="#">Comunicação</a></p>
                                 </div>
                                 <div class="col-md-12 ml-3 border-left-footer">
-                                    <p>Endomarketing</p>
+                                    <p><a href="#">Endomarketing</a></p>
                                 </div>
                                 <div class="col-md-12 ml-3 border-left-footer">
-                                    <p>Gestão de Pessoas</p>
+                                    <p><a href="#">Gestão de Pessoas</a></p>
                                 </div>
                             </div>
                         </div>
@@ -85,8 +85,8 @@
                                 <h2>nas redes sociais</h2>
                                 <div class="row">
                                     <div class="m-auto">
-                                        <i class="fab fa-facebook-square icon-footer"></i>
-                                        <i class="fab fa-youtube ml-4 icon-footer"></i>
+                                        <a class="color-footer" href="#"><i class="fab fa-facebook-square icon-footer"></i></a>
+                                        <a class="color-footer" href="#"><i class="fab fa-youtube ml-4 icon-footer"></i></a>
                                     </div>
                                 </div>
                             </div>
@@ -103,27 +103,27 @@
                             <div class="col-lg-2 border-right-footer">
                                 <div class="row">
                                     <div class="ml-auto border-radius-footer"></div>
-                                    <span class="mr-auto text-light">Status do sistema</span>
+                                    <span class="mr-auto"><a class="color-footer" href="#">Status do sistema</a></span>
                                 </div>
                             </div>
                             <div class="col-lg-2 border-right-footer">
-                                <span class="d-flex justify-content-center text-light">Segurança</span>
+                                <span class="d-flex justify-content-center text-light"><a href="#">Segurança</a></span>
                             </div>
                             <div class="col-lg-2 border-right-footer">
-                                <span class="d-flex justify-content-center text-light">Termos de Uso</span>
+                                <span class="d-flex justify-content-center text-light"><a href="#">Termos de Uso</a></span>
                             </div>
                             <div class="col-lg-2 border-right-footer">
-                                <span class="d-flex justify-content-center text-light">Privacidade</span>
+                                <span class="d-flex justify-content-center text-light"><a href="#">Privacidade</a></span>
                             </div>
                             <div class="col-lg-2 border-right-footer">
-                                <span class="d-flex justify-content-center text-light">Sobre Nós</span>
+                                <span class="d-flex justify-content-center text-light"><a href="#">Sobre Nós</a></span>
                             </div>
                             <div class="col-lg-2">
-                                <span class="d-flex justify-content-center text-light">Contato</span>
+                                <span class="d-flex justify-content-center text-light"><a class="color-footer" href="#">Contato</a></span>
                             </div>
                         </div>
                     </div>
-                    <div class="col-lg-2 mt-4 mb-4">
+                    <div class="col-lg-2 mt-4 mb-4 border-footer">
                         <div class="row">
                             <div class="m-auto">
                                 <img src="https://www.vivaintra.com/media/images/v2/vivaweb.png" alt="">
