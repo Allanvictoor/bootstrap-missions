1130e7e66150ad56fe2deda4f94fd62f58ac70ee
allanvictoor <allanbatista2010@gmail.com>
2020-04-17 17:55:00 -0300

    melhorando projeot de missão

---
+++ missoes/bootstrap/allan/js/jquery.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+
+
+
+  $('#imgLogin').hide()
+
+  $('#btnLogar').click(function(){
+    $('#formLogin').hide();
+    $('#imgLogin').fadeIn();
+  })
+  
+
+  
+  $('#bemvindo').hide()
+  $('#btnLogar').click(function(){
+    $('#containerHead').fadeOut();
+    $('#facaLogin').fadeIn();
+  })
+  
+
+  });
+  
