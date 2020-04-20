f5f9797341fc1b84d529c35a1f7b5202ea9e2dd3
allanvictoor <allanbatista2010@gmail.com>
2020-04-20 15:09:05 -0300

    ultimas melhorias

--- missoes/bootstrap/allan/js/jquery.js
+++ missoes/bootstrap/allan/js/jquery.js
@@ -1,35 +1,33 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
 
 
-
+//Efeito no Dropdown apos Login
   $('#imgLogin').hide()
-
-  $('#btnLogar').click(function(){
+  $('#btnLogar').click(function () {
     $('#formLogin').hide();
     $('#imgLogin').fadeIn();
   })
-  
 
-  
+
+//Evento do No body apos Logar
   $('#bemvindo').hide()
-  $('#btnLogar').click(function(){
+  $('#btnLogar').click(function () {
     $('#containerHead').fadeOut();
     $('#facaLogin').fadeIn();
   })
 
 
-
+//Evento do Modal de Cadastro
   $('#modalTextoDois').hide()
   $('#imgEmailModal').hide()
-  $('#btnModal').click(function(){
+  $('#btnModal').click(function () {
     $('#modalTextoUm').hide()
     $('#modalTextoDois').fadeIn()
     $('#formModal').hide()
     $('#imgEmailModal').fadeIn()
     $('#btnbtnmodal').hide()
   });
-  
 
-  });
-  
+
+});
