6262773941bc5f23eaa19cb6f5852b7347878dcc
allanbatista-vwi <allanbatista@vivaweb.net>
2020-06-23 16:50:16 -0300

    correções no gabarito e melhorias

--- missoes/bootstrap/allan/gabarito/sejaaluno.php
+++ missoes/bootstrap/allan/gabarito/sejaaluno.php
@@ -9,25 +9,39 @@
   include('../gabarito/template/nav.php')
   ?>
 
-<div class="container-fluid">
-    <div class="container">
+<div class="container-fluid colorbody">
+    <div id="sejaAluno" class="container">
         <div class="row">
-            <div class="col-sm my-auto">
-                <h2>Já tem cadastro? Faça o Login</h2>
+            <div class="col-sm pt-5">
+                <h3 class="text-center pb-5">Já tem cadastro? Faça o Login</h3>
+                <form class="pt-5">
+                  <div class="form-group w-75 ml-5">
+                    <label for="exampleInputEmail1">Endereço de Email</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                  </div>
+                  <div class="form-group w-75 ml-5">
+                    <label for="exampleInputPassword1">Senha</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1">
+                  </div>
+
+                  <button type="submit" class="btn btn-primary w-75 ml-5">Entrar</button>
+              </form>
                 
             </div>
+            <div id="borderSejaAluno" class="m-5"></div>
             <div class="col-sm">
-
+                <h3 class="text-center pt-5">
+                  Cadastre-se
+                </h3>
                 </div>
         </div>
     </div>
 </div>
 
-
+</body>
 
 
 <?php
   include('../gabarito/template/footer.php')
   ?>
 
-  </body>
